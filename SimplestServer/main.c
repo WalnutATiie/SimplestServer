b950fa9910d7732639259974f544b7cdbc83bd07
李抗 <likang@localhost>
2016-07-10 20:58:28 +0800

    Initial Commit

---
+++ SimplestServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SimplestServer
+//
+//  Created by 李抗 on 7/10/16.
+//  Copyright © 2016 李抗. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
